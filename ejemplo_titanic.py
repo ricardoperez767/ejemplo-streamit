d1a407fc7b48cb3cc1c371092ec0a97ab9ff94ac
ricardoperez767 <ricardoperezbazan82@gmail.com>
2025-11-17 15:19:31 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -53,35 +53,6 @@ st.write("""
 st.table(df.head())
 
 # ---------------------------------------
-# MODIFICACIÓN: de (1,2) a (1,3) + ancho 15
-# ---------------------------------------
-fig, ax = plt.subplots(1, 3, figsize=(15, 3))
-
-# ---------------------------------------
-# Gráfico 1: Histograma de edades
-# ---------------------------------------
-div = 10   # número de divisiones para los bins
-
-ax[0].hist(df["Age"], bins=div)
-ax[0].set_xlabel("Edad")
-ax[0].set_ylabel("Frecuencia")
-ax[0].set_title("Histograma de edades")
-
-# ---------------------------------------
-# Gráfico 2: Total hombres y mujeres
-# ---------------------------------------
-df_male = df[df["Sex"] == "male"]
-df_female = df[df["Sex"] == "female"]
-
-cant_male = len(df_male)
-cant_female = len(df_female)
-
-ax[1].bar(["Masculino", "Femenino"], [cant_male, cant_female], color="red")
-ax[1].set_xlabel("Sexo")
-ax[1].set_ylabel("Cantidad")
-ax[1].set_title("Distribución de hombres y mujeres")
-
-# ---------------------------------------
 # Gráfico 3: Sobrevivientes agrupados por sexo
 # ---------------------------------------
 sob_male = len(df[(df["Sex"] == "male") & (df["Survived"] == 1)])
@@ -94,9 +65,3 @@ ax[2].set_title("Sobrevivientes por Sexo")
 
 # Mostrar gráfico
 st.pyplot(fig)
-
-# ---------------------------------------
-# Mostrar tabla
-# ---------------------------------------
-st.write("## Muestra de datos cargados")
-st.table(df.head())
